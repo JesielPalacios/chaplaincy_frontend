bfe70e842efb93ee08d4e5efd3315c3e30e92b7e
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-09-28 11:56:55 -0500

    Adds latest changes

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -9,6 +9,7 @@ const LogIn = React.lazy(() => import('./components/Login/Login'))
 const Dashboard = React.lazy(() => import('./components/Dashboard/Dashboard'))
 const CustomersList = React.lazy(() => import('./components/CustomersList'))
 const Customer = React.lazy(() => import('./components/Customer'))
+const CustomerAddOrEdit = React.lazy(() => import('./components/CustomerAddOrEdit'))
 const Interviews = React.lazy(() => import('./components/Interviews/Interviews'))
 
 export const AppRouter = () => {
@@ -22,12 +23,17 @@ export const AppRouter = () => {
           <Route path="/" element={<Home />} />
           <Route path="login" element={!isAuth ? <LogIn /> : <Navigate replace to="/" />} />
           <Route path="dashboard" element={isAuth ? <Dashboard /> : <Navigate replace to="/login" />} />
-          {/* <Route path="usuarios" element={isAuth ? <Users /> : <Navigate replace to="/login" />} /> */}
-          {/* <Route path="usuarios/:userId" element={isAuth ? <User /> : <Navigate replace to="/login" />} /> */}
-          <Route path="beneficiarios" element={isAuth ? <CustomersList /> : <Navigate replace to="/login" />} />
-          <Route path="beneficiarios/:customerId" element={isAuth ? <Customer /> : <Navigate replace to="/login" />} />
+
+          <Route path="beneficiarios">
+            <Route index element={isAuth ? <CustomersList /> : <Navigate replace to="/login" />} />
+            <Route path=":customerId" element={isAuth ? <Customer /> : <Navigate replace to="/login" />} />
+            <Route path="nuevo" element={isAuth ? <CustomerAddOrEdit title="Crear nuevo beneficiario" /> : <Navigate replace to="/login" />} />
+            <Route path="editar">
+              <Route path=":customerIdForEdit" element={isAuth ? <CustomerAddOrEdit title="Editar beneficiario" /> : <Navigate replace to="/login" />} />
+            </Route>
+          </Route>
+
           <Route path="entrevistas" element={isAuth ? <Interviews /> : <Navigate replace to="/login" />} />
-          {/* <Route path="entrevistas/:interviewId" element={isAuth ? <User /> : <Navigate replace to="/login" />} /> */}
         </Routes>
       </Router>
     </Suspense>
